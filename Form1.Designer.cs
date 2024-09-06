ab62e0796ff8a479640c1221f7979994e343428a
Marcus Rossen <142300424+MarcusRossen@users.noreply.github.com>
2024-09-06 12:39:35 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Spil
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.start_textbox = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.answer_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // start_textbox
+            // 
+            this.start_textbox.Font = new System.Drawing.Font("Microsoft Sans Serif", 40.25F);
+            this.start_textbox.Location = new System.Drawing.Point(358, 193);
+            this.start_textbox.Name = "start_textbox";
+            this.start_textbox.ReadOnly = true;
+            this.start_textbox.Size = new System.Drawing.Size(68, 68);
+            this.start_textbox.TabIndex = 0;
+            this.start_textbox.Text = " ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 40.25F);
+            this.textBox2.Location = new System.Drawing.Point(358, 267);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(68, 68);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress_1);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 40.25F);
+            this.textBox3.Location = new System.Drawing.Point(432, 193);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(68, 68);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = " ";
+            this.textBox3.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox3_KeyPress);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Microsoft Sans Serif", 40.25F);
+            this.textBox4.Location = new System.Drawing.Point(358, 119);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(68, 68);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.Text = " ";
+            this.textBox4.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox4_KeyPress);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 40.25F);
+            this.textBox5.Location = new System.Drawing.Point(284, 193);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(68, 68);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.Text = " ";
+            this.textBox5.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox5_KeyPress);
+            // 
+            // answer_button
+            // 
+            this.answer_button.Location = new System.Drawing.Point(640, 357);
+            this.answer_button.Name = "answer_button";
+            this.answer_button.Size = new System.Drawing.Size(100, 50);
+            this.answer_button.TabIndex = 9;
+            this.answer_button.Text = "Svar";
+            this.answer_button.UseVisualStyleBackColor = true;
+            this.answer_button.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.answer_button);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.start_textbox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox start_textbox;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button answer_button;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Spil
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            GetRandomNumber();
+        }
+
+        public int RandomNumber{ get; set; }
+
+        private void GetRandomNumber()
+        {
+            Random rnd = new Random();
+            RandomNumber = rnd.Next(11, 89);
+            start_textbox.Text = RandomNumber.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt32(textBox2.Text) == RandomNumber - 10 && Convert.ToInt32(textBox4.Text) == RandomNumber + 10 && Convert.ToInt32(textBox3.Text) == RandomNumber + 1 && Convert.ToInt32(textBox5.Text) == RandomNumber - 1)
+            {
+                MessageBox.Show("Tillykke du har klaret spillet");
+            }
+            else
+            {
+                MessageBox.Show("Du har tabt");
+            }
+
+            RestartGame();
+        }
+
+        private void RestartGame()
+        {
+            textBox2.Text = string.Empty;
+            textBox3.Text = string.Empty;
+            textBox4.Text = string.Empty;
+            textBox5.Text = string.Empty;
+
+            GetRandomNumber();
+        }
+
+        private void textBox2_KeyPress_1(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsNumber(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsNumber(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsNumber(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void textBox5_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsNumber(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+    }
+}
